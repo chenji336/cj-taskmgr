63f54a36e604c8a4d19bc026d05f3bb885b1b719
前端开发部 - 陈骥 <chenji@jiguang.cn>
2018-10-29 14:29:36 +0800

    7-9 task reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -12,13 +12,17 @@ import * as fromQuote from './quote.reducer';
 import * as fromAuth from './auth.reducer';
 import * as fromProject from './project.reducer';
 import * as fromTaskList from './task-list.reducer';
+import * as fromTask from './task.reducer';
+import * as fromUser from './user.reducer';
 
 
 export interface State {
     quote: fromQuote.State,
     auth: Auth,
     projects: fromProject.State,
-    taskLists: fromTaskList.State
+    taskLists: fromTaskList.State,
+    tasks: fromTask.State,
+    users: fromUser.State,
 }
 
 const initialState: State = {
@@ -26,6 +30,8 @@ const initialState: State = {
     auth: fromAuth.initialState,
     projects: fromProject.initialState,
     taskLists: fromTaskList.initialState,
+    tasks: fromTask.initialState,
+    users: fromUser.initialState,
 };
 
 const reducers = {
@@ -33,6 +39,8 @@ const reducers = {
     auth: fromAuth.reducer,
     projects: fromProject.reducer,
     taskLists: fromTaskList.reducer,
+    tasks: fromTask.reducer,
+    users: fromUser.reducer,
 };
 
 // 测试环境嵌入这些插件，storeFreeze也是
@@ -62,11 +70,37 @@ export const getQuoteState = (state: State) => {
 export const getAuthState = (state: State) => state.auth;  // 这里直接获取的就是auth的值，不需要auth.auth这样
 export const getProjectsState = (state: State) => state.projects;
 export const getTaskListState = (state: State) => state.taskLists;
+export const getTasksState = (state: State) => state.tasks;
+export const getUsersState = (state: State) => state.users;
 
 // 如果成功了可以测试一下@ngrx/store的createSelector是不是也是一样的效果
 export const getQuote = createSelector(getQuoteState, fromQuote.getQuote);
 export const getProjects = createSelector(getProjectsState, fromProject.getAll);
 export const getTaskLists = createSelector(getTaskListState, fromTaskList.getSelected);
+export const getTasks = createSelector(getTasksState, fromTask.getTasks);
+export const getUsers = createSelector(getUsersState, fromUser.getUsers);
+
+export const getUserEntities = createSelector(getUsersState, fromUser.getEntities);
+export const getTasksWithOwners = createSelector(getTasks, getUserEntities, (tasks, userEntities) => {
+    return tasks.map(task => ({
+        ...task,
+        owner: userEntities[task.ownerId],
+        participants: task.participantIds.map(id => userEntities[id])
+    }))
+});
+export const getTasksByLists = createSelector(getTaskLists, getTasksWithOwners, (lists, tasks) => {
+    return lists.map(list => ({
+        ...list,
+        tasks: tasks.filter(task => task.taskListId === list.id)
+    }))
+});
+export const getProjectUsers = (projectId: string) => createSelector(
+    getProjectsState, 
+    fromUser.getEntities, 
+    (state, userEntities) => {
+        return state.entities[projectId].memebers.map(id => userEntities[id]);
+    }
+);
 
 @NgModule({
     imports: [
---
+++ src/app/reducers/task.reducer.ts
@@ -0,0 +1,111 @@
+import * as _ from 'lodash';
+import { createSelector } from 'reselect';
+import * as prjActions from '../actions/project.action';
+import * as actions from '../actions/task.action';
+import { Project, Task } from './../domain';
+
+export interface State {
+     ids: string[];
+     entities: {[id: string]: Task};
+}
+
+export const initialState: State = {
+    ids: [],
+    entities: {},
+};
+
+const addTask = (state, action) => {
+  const taskList = action.payload;
+  if (state.entities[taskList.id]) {
+    return state;
+  }
+  const newIds = [...state.ids, taskList.id];
+  const newEntities = {...state.entities, [taskList.id]: taskList};
+  return {...state, ids: newIds, entities: newEntities};
+};
+
+const updateTask = (state, action) => {
+  const taskList = action.payload;
+  const newEntities = {...state.entities, [taskList.id]: taskList};
+  return {...state, entities: newEntities};
+};
+
+const delTask = (state, action) => {
+  const taskList = action.payload;
+  const newIds = state.iods.filter(id => id !== taskList.id);
+  const newEntities = newIds.reduce((entities, id: string) => ({...entities, [id]: state.entities[id]}), {});
+  return {
+    ids: newIds,
+    entities: newEntities,
+  };
+};
+
+const loadTasks = (state, action) => {
+  const tasks = action.payload;
+  const incomingIds = tasks.map(p => p.id);
+  const newIds = _.difference(incomingIds, state.ids);
+  const incomingEntities = _.chain(tasks)
+    .keyBy('id')
+    .mapValues(o => o)
+    .value();
+  const newEntities = newIds.reduce((entities, id: string) => ({ ...entities, [id]: incomingEntities[id] }), {});
+  return {
+    ids: [...state.ids, ...newIds],
+    entities: {...state.entities, ...newEntities},
+  };
+};
+
+const moveAllTasks = (state, action) => {
+  const tasks = <Task[]>action.payload;
+  const updatedEntities = tasks.reduce((entities, task) => ({...entities, [task.id]: task}), {});
+  return {
+    ...state,
+    entities: {...state.entities, ...updatedEntities}
+  };
+};
+
+const delByPrj = (state, action) => {
+  const project = <Project>action.payload;
+  const taskListIds = project.taskLists;
+  const remainingIds = state.ids.filter(id => taskListIds.indexOf(state.entities[id].taskListId) === -1);
+  const remainingEntities = remainingIds.reduce((entities, id) => ({...entities, [id]: state.entitites}), {});
+  return {
+    ids: [...remainingIds],
+    entities: remainingEntities,
+  };
+};
+
+export function reducer(state = initialState, action: actions.Actions ): State {
+  switch (action.type) {
+    case actions.ActionTypes.ADD_SUCCESS: {
+      return addTask(state, action);
+    }
+    case actions.ActionTypes.DELETE_SUCCESS: {
+      return delTask(state, action);
+    }
+    case actions.ActionTypes.COMPLETE_SUCCESS:
+    case actions.ActionTypes.MOVE_SUCCESS:
+    case actions.ActionTypes.UPDATE_SUCCESS: {
+      return updateTask(state, action);
+    }
+    case actions.ActionTypes.LOAD_SUCCESS: {
+      return loadTasks(state, action);
+    }
+    case actions.ActionTypes.MOVE_ALL_SUCCESS: {
+      return moveAllTasks(state, action);
+    }
+    case prjActions.ActionTypes.DELETE_SUCCESS: {
+      return delByPrj(state, action);
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export const getIds = (state: State) => state.ids;
+export const getEntities = (state: State) => state.entities;
+
+export const getTasks = createSelector(getIds, getEntities, (ids, entities) => {
+  return ids.map(id => entities[id]);
+});
